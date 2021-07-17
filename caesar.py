0bbf15c6bf3afc91d26637be9fecbe99c0c4732b
Anthony Ramirez <64566330+t-ramz@users.noreply.github.com>
2021-07-17 11:35:42 -0500

    Squashed commit of the following:
    commit 018d3d280b89386459a07b65b511241bd8cc45e6
    Author: Anthony Ramirez <64566330+t-ramz@users.noreply.github.com>
    Date:   Sat Jul 17 11:29:30 2021 -0500
    
        Add docstrings
    
    commit 7930cf0eea10e411c983d43067fc028e074ae6ea
    Author: Anthony Ramirez <64566330+t-ramz@users.noreply.github.com>
    Date:   Sat Jul 17 11:17:03 2021 -0500
    
        Add option to decipher
    
    commit d37d523bb996fe7cdab2fac28a893434eb0d9f5e
    Author: Anthony Ramirez <64566330+t-ramz@users.noreply.github.com>
    Date:   Sat Jul 17 11:06:35 2021 -0500
    
        Handle some special characters by re-appending them

--- caesar.py
+++ caesar.py
@@ -1,9 +1,20 @@
 class Caesar:
+    special_chars = [' ', '!', ',', '.', '?', "'"]
     def __init__(self, offset: str):
+        """
+        Accepts string for character mapping in the form of 'a=b'
+
+        Parameters:
+        -----------
+        offset: str
+            character mapping for offset
+            must be a single string with 3 characters:
+                input, '=', and output
+        """
         self.offset: str = offset
         self.int_offset = None
 
-    def _calc_offset_int(self):
+    def _calc_offset_int(self): # currently lazy offset calculation
         plaintext, _, ciphertext = self.offset.partition('=')
         plaintext.lower()
         ciphertext.lower()
@@ -17,18 +28,39 @@ class Caesar:
             raise
         self.int_offset = abs(int_plaintext-int_ciphertext)
 
-    def encipher(self, plaintext: str) -> str:
+    def process(self, in_text: str, decipher: bool = False) -> str:
+        """
+        This method will process a text string. By default it will encipher with the object's offset, but it also handles decipher.
+
+        Parameters:
+        -----------
+        in_text: str
+            the input text to be processed by the caesar cipher
+
+        decipher: bool = False
+            optional configuration to decipher an input ciphertext
+
+        Returns:
+        --------
+        out_text: str
+            resulting string from processing, will be either ciphertext or plaintext
+        """
         if not self.int_offset:
             self._calc_offset_int()
-        ciphertext: str = ""
-        for character in plaintext: # not currently handling special characters at all
-            if character.isupper():
-                base = ord('A')
+        out_text: str = ""
+        for character in in_text:
+            if character in self.special_chars:
+                # TODO: actually encipher this
+                out_text = out_text + character
             else:
-                base = ord('a')
-            normalized_ascii_plain = ord(character) - base
-            normalized_acii_cipher = (normalized_ascii_plain + self.int_offset) % 26
-            actual_acii_cipher = normalized_acii_cipher + base
-            ciphertext = ciphertext + chr(actual_acii_cipher)
+                if character.isupper():
+                    base = ord('A')
+                else:
+                    base = ord('a')
+                normalized_ascii_plain = ord(character) - base
+                offset = self.int_offset if not decipher else 0-self.int_offset
+                normalized_acii_cipher = (normalized_ascii_plain + offset) % 26
+                actual_acii_cipher = normalized_acii_cipher + base
+                out_text = out_text + chr(actual_acii_cipher)
 
-        return ciphertext
+        return out_text
